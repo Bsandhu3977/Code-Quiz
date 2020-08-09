fd8ccc4fdf0d62fd3f6b7a023ef90afe1deaff62
Baljinder Sandhu <b.sandhu3977@gmail.com>
2020-08-08 23:45:33 -0700

    beginning of code

---
+++ script.js
@@ -0,0 +1,12 @@
+//create variables that select onto elements in HTML 
+
+//function?? or for loop?? through list of questions??? 
+
+//var score
+//var secondsLeft
+
+//function that runs through each ques w/ conditionals on whether answered correct or not. 
+//according ++ or -- and add that to score 
+//preventDefault each time load. 
+//store answers in local storage 
+//when user click answer, display conditional on bottom 
